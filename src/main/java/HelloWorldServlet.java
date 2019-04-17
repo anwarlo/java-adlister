9ebe7b56acb9c402043fd3445f304435d7ec98d6
Anwar Lopez <anwarlo247@gmail.com>
2019-04-17 10:15:32 -0500

    in class exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,17 +5,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+@WebServlet(urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+
+   int counter = 0;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        System.out.println("Hello World servlet has run");
+
 //        response.getWriter().println("<h1>Hello, World!</h1>");
+
+        counter++;
+
         String name = request.getParameter("name");
 
         if (name == null) name = "World";
 
         request.setAttribute("name",name);
+        request.setAttribute("count",counter);
         request.getRequestDispatcher("/hello.jsp").forward(request,response);
-
 
     }
 }
--- src/main/webapp/hello.jsp
+++ src/main/webapp/hello.jsp
@@ -1,11 +1,14 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ include file="/partials/head.jsp" %>
-<%@ include file="/partials/navbar.jsp" %><html>
-<head>
-    <title>Title</title>
-</head>
-<body>
 
+<html>
+<head>
+    <title>Hello</title>
+    <%@ include file="/partials/head.jsp" %></head>
+<body>
+<%@ include file="/partials/navbar.jsp" %>
+<h1> The current count is ${count}.</h1>
+
+<h2>Hello, ${name}!</h2>
 </body>
 </html>
